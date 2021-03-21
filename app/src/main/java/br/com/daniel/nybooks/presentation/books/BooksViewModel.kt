38b60b8e9b832ef7d685a1b98ca0a8000d63ba7e
SilvaTs <danielsilvatsi@outlook.com>
2021-03-21 12:43:24 -0400

    feat: requisição getLista Título e Autor do livro via retrofit

--- app/src/main/java/br/com/daniel/nybooks/presentation/books/BooksViewModel.kt
+++ app/src/main/java/br/com/daniel/nybooks/presentation/books/BooksViewModel.kt
@@ -2,22 +2,46 @@ package br.com.daniel.nybooks.presentation.books
 
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import br.com.daniel.nybooks.data.ApiService
 import br.com.daniel.nybooks.data.model.Book
+import br.com.daniel.nybooks.data.response.BookBodyResponse
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class BooksViewModel : ViewModel() {
 
     val booksLiveData: MutableLiveData<List<Book>> = MutableLiveData()
 
     fun getBooks() {
-       booksLiveData.value = createFakeBooks()
+
+       ApiService.service.getBooks().enqueue(object: Callback<BookBodyResponse>{
+
+           override fun onResponse(call: Call<BookBodyResponse>, response: Response<BookBodyResponse>) {
+                if (response.isSuccessful) {
+                     val books: MutableList<Book> = mutableListOf()
+
+                    response.body()?.let {bookBodyResponse ->
+                        for (resultResponse in bookBodyResponse.bookResults) {
+                              val book = Book(
+                                      title = resultResponse.bookDetailsResponses[0].title,
+                                      author = resultResponse.bookDetailsResponses[0].author
+                              )
+
+                            books.add(book)
+                        }
+
+                    }
+
+                    booksLiveData.value = books
+                }
+           }
+
+           override fun onFailure(call: Call<BookBodyResponse>, t: Throwable) {
+
+           }
+
+       })
     }
 
-    fun createFakeBooks(): List<Book> {
-        return listOf(
-            Book("Title 1", "Author 1"),
-            Book("Title 2", "Author 2"),
-            Book("Title 3", "Author 3"),
-            Book("Title 4", "Author 4")
-        )
-    }
 }
